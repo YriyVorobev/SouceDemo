e53bc5a38c1b66619ab64ce3750c97c1f1449559
unvhookah@icloud.com <unvhookah@icloud.com>
2026-01-19 22:00:13 +0300

    добавлен тест на удаления товара из корзины

--- base/base_test.py
+++ base/base_test.py
@@ -1,5 +1,7 @@
 from data.credentials import Credentials
 from pages.login.login import LoginPage
+from pages.products_to_cart.prodeucts_to_cart import ProductsToCart
+from pages.delete_cart_products.delete_cart_products import DeleteCartProduct
 
 
 class BaseTest:
@@ -7,3 +9,5 @@ class BaseTest:
     def setup_method(self):
         self.credentials = Credentials()
         self.login_page = LoginPage(self.driver)
+        self.products_page = ProductsToCart(self.driver)
+        self.delete_cart_products = DeleteCartProduct(self.driver)
